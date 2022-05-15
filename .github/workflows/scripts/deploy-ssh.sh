46c600c284649f8e8cf825a9780a23e5d0f1d7bf
s.elmanov <s.elmanov@aventica.ru>
2022-05-15 08:40:54 +0300

    update workflow

--- .github/workflows/scripts/deploy-ssh.sh
+++ .github/workflows/scripts/deploy-ssh.sh
@@ -1,20 +1,21 @@
+#!/bin/sh
+
 GREEN='\033[0;32m'
 REG='\033[0;31m'
 NC='\033[0m' # No Color
 
-echo -e []${GREEN}docker pull new container
+echo -e [] ${GREEN}Docker pull new container
 sudo docker pull cr.yandex/$CR_REGISTRY/$CR_REPOSITORY:$IMAGE_TAG
 
-echo -e []Search running containers
+echo -e [] Search running containers
 containerIds=$(sudo docker ps -a -q --filter "name=Frontend")
 
-echo -e []Running containers: ${GREEN}$containerIds
+echo -e [] Running containers: ${GREEN}$containerIds
 if [[ -n $containerIds ]] ; then
-  echo -e []${GREEN} Delete containers ${RED} $containerIds
-  sudo docker stop $containerIds
+  echo -e [] Delete containers ${RED}$(sudo docker stop $containerIds)
 fi
 
-echo -e []Run ${GREEN}cr.yandex/$CR_REGISTRY/$CR_REPOSITORY:$IMAGE_TAG ${NC}WITH NAME ${GREEN}Frontend.$GITHUB_RUN_NUMBER
+echo -e [] Run ${GREEN}cr.yandex/$CR_REGISTRY/$CR_REPOSITORY:$IMAGE_TAG ${NC}WITH NAME ${GREEN}Frontend.$GITHUB_RUN_NUMBER
 sudo docker run --name Frontend.$GITHUB_RUN_NUMBER -d -p 80:80 cr.yandex/$CR_REGISTRY/$CR_REPOSITORY:$IMAGE_TAG
 
 sudo docker system prune -a -f
